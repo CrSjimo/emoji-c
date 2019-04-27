962cbeb20270723707ba556a82ac6c2f3784f4cd
Crindzebra <crs_16423@outlook.com>
2019-04-27 20:52:12 +0800

    init

---
+++ emojic.h
@@ -0,0 +1,62 @@
+#ifndef EMOJI_C
+#define EMOJI_C
+
+#define 🔢 int
+#define 🔆 long
+#define 🔅 short
+#define 🌊🔢 float
+#define 🌊🌊🔢 double
+#define 🆗 _Bool
+#define 🔠 char
+#define 📌 signed
+#define 📍 unsigned
+#define 🈳🈚 void
+#define ℹ️ sizeof
+#define 🗒️ struct
+#define 📝 union
+#define 📇 enum
+#define 🗃️ const
+#define 🏃 goto
+
+
+#define 🈶 1
+#define 🈚 0
+#define 👆 return
+#define 👈 break
+#define 👇 continue
+#define 👉 do
+#define 👌 if
+#define 🤙 else
+#define 🔄 while
+#define 🔃 for
+#define 👍 switch
+#define 👋 case
+#define 🙌 default 
+
+
+//in <stdio.h>
+#define 📥📮 scanf
+#define 📤📮 printf
+#define 📄📥📮 fscanf
+#define 📄📤📮 fprintf
+#define 🔗📥📮 sscanf
+#define 🔗📤📮 sprintf
+#define 📥📨 getchar
+#define 📤📨 putchar
+#define 📥🔗 gets
+#define 📤🔗 puts
+#define 🗳️ perror
+#define 📄 FILE
+#define 📄📬 fopen
+#define 📄📪 fclose
+#define 📄🗑️ remove
+#define 📄📎 rename
+#define 🔚 EOF
+
+//in <stdlib.h>
+#define 🈳🈯 NULL
+#define 🔗🌊🔢 atof
+#define 🔗🔢 atoi
+#define 🔗🔆🔢 atol
+
+#endif
